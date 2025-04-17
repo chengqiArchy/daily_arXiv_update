414fd324d78e1dc8f1c7411d6329b2289923a3b1
CHENGQI <arhyzhengchengqi@gmail.com>
2025-04-18 00:34:16 +0800

    init

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 today=`date -u "+%Y-%m-%d"`
-# cd daily_arxiv
-# scrapy crawl arxiv -o ../data/${today}.jsonl
+cd daily_arxiv
+scrapy crawl arxiv -o ../data/${today}.jsonl
 
-cd ai
-python enhance.py --data ../data/2025-04-17.jsonl
+cd ../ai
+python enhance.py --data ../data/${today}.jsonl
 
 cd ../to_md
 python convert.py --data ../data/${today}_AI_enhanced_${LANGUAGE}.jsonl
